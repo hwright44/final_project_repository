49b62af5a3eec080d5cc521781c3f34e88f0e2d5
Hunter Wright <hwright4@terpmail.umd.edu>
2021-11-14 17:34:09 -0500

    added function that makes duck appear randomly

--- FQuader_grid.py
+++ FQuader_grid.py
@@ -1,3 +1,5 @@
+import random
+
 board = []
 
 for i in range(0,10):
@@ -7,3 +9,15 @@ def print_board(board):
     for row in board:
         print(" ".join(row))
 print_board(board)
+
+
+def rand_duck(cells):
+        """Places a "duck" in a random position on the board
+        Args:
+        """
+        duck = "\u26BD"
+        cells = [["0"] * 10 for i in range(10)]
+        for i in range(4):
+                x_coordinate = random.randrange(10)
+                y_coordinate = random.randrange(10)
+                cells[x_coordinate][y_coordinate] = duck
