178cd8b3d1417ec4f078c07cdc5a22d7e70ec337
farhanquader <farhan.quader@technuf.com>
2021-11-15 22:11:10 -0500

    Added all docstrings

--- FQuader_grid.py
+++ FQuader_grid.py
@@ -7,8 +7,13 @@ for i in range(0,10):
         board.append("_|_|" * 10)
     
 def print_board(board):
-    for row in board:
-        print(" ".join(row))
+        """     Displays an output of a grid from the list variable 'board'
+                
+                Args: List - board
+                
+                Side effects: Output displays a grid"""
+        for row in board:
+                print(" ".join(row))
 print_board(board)
 
 
