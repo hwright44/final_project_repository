069ec1667ff372a80edd26b380638f78cb7b76da
Hunter Wright <hwright4@terpmail.umd.edu>
2021-12-01 13:10:31 -0500

    intial changes to Farhan's grid

--- FQuader_grid.py
+++ FQuader_grid.py
@@ -10,16 +10,3 @@ def print_board(board):
     for row in board:
         print(" ".join(row))
 print_board(board)
-
-
-def rand_duck():
-        """Places a "duck" in a random position on the board
-        Side effects: prints grid to the console
-        """
-        duck = "\u26BD"
-        cells = [["0"] * 10 for i in range(10)]
-        for i in range(4):
-                x_coordinate = random.randrange(10)
-                y_coordinate = random.randrange(10)
-                cells[x_coordinate][y_coordinate] = duck
-        pprint.pprint(cells)
